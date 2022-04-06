52d986135f983a0c64b6aa59c85727ff34d98a92
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-04-06 13:18:16 +0530

    fix: pr changes

---
+++ src/Components/Sidenav/Sidenav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./sidenav.css";
+
+const Sidenav = () => {
+
+    return (
+        <aside className="filters-container light-txt">
+
+        </aside>
+
+    )
+}
+
+export { Sidenav }
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
-
+import { Sidenav } from "../../Components/Sidenav/Sidenav"
+import "./homepage.css"
+import { Navbar } from '../../Components/Navbar/Navbar'
 const HomePage = () => {
     return (
-        <div>
-            <h1>Template Routing Common ui</h1>
+        <div className="products-layout">
+            <Navbar />
+            <Sidenav />
             <Link to="/test1">test1</Link>
             <Link to="/test2">test2</Link>
             <Link to="/test3">test3</Link>
