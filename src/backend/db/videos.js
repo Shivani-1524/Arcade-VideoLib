1daeed4d2ccb21f3d4d250ea495ae5c5659e9c00
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2022-04-08 16:13:15 +0530

    update: videos database

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -10,10 +10,10 @@ export const videos = [
     _id: uuidv4(),
     title: "Valorant Mobile Gameplay Testing",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "The beta version of valorant mobile testing is recorded along with reviews and eggWicks opinions",
     videoUrl: "https://www.youtube.com/watch?v=bPGLPCXDaFI",
     channelName: "eggWick",
-    channelImg: "https://yt3.ggpht.com/Qvy3LrbqlBve3OrtJHHCK9cXyBgTA5weV-vJ04Cy_gY9nLHISmM0gDGBhhGU1AmPFilKa33WeA=s48-c-k-c0x00ffffff-no-rj",
+    channelImg: "https://yt3.ggpht.com/ytc/AKedOLSShE9GXnbzAo-i_oYLzINXaIQ2j7aKXxLikq-P=s68-c-k-c0x00ffffff-no-rj",
     category: "New Updates",
     thumbnail: "https://img.youtube.com/vi/bPGLPCXDaFI/maxresdefault.jpg"
 
@@ -22,40 +22,55 @@ export const videos = [
     _id: uuidv4(),
     title: "2 Pros Vs 5 Noobs in Valorant",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "The pros playing 2 versus 5 against noobs in valorant, lets see if its experience or quantity that finally wins",
     videoUrl: "https://www.youtube.com/watch?v=oJSdnwbFLVo",
     channelName: "TenZ",
     category: "Funny Clips",
-    thumbnail: "https://img.youtube.com/vi/oJSdnwbFLVo/maxresdefault.jpg"
+    thumbnail: "https://img.youtube.com/vi/oJSdnwbFLVo/maxresdefault.jpg",
+    channelImg: "https://yt3.ggpht.com/Qvy3LrbqlBve3OrtJHHCK9cXyBgTA5weV-vJ04Cy_gY9nLHISmM0gDGBhhGU1AmPFilKa33WeA=s48-c-k-c0x00ffffff-no-rj",
   },
   {
     _id: uuidv4(),
-    title: "2 Pros Vs 5 Noobs in Valorant",
+    title: "Collected Miscellany - Kamisato Ayato",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    videoUrl: "https://www.youtube.com/watch?v=oJSdnwbFLVo",
-    channelName: "TenZ",
-    category: "Funny Clips",
-    thumbnail: "https://img.youtube.com/vi/oJSdnwbFLVo/maxresdefault.jpg"
+      "Kamisato Ayato, one of the new characters to grace inazuma. This video talks about his abilities and his story.",
+    videoUrl: "https://www.youtube.com/watch?v=E7wDRANQI8I",
+    channelName: "Genshin Impact",
+    category: "New Updates",
+    thumbnail: "https://img.youtube.com/vi/E7wDRANQI8I/maxresdefault.jpg",
+    channelImg: "https://yt3.ggpht.com/7FlKwMyotApIow-_l6-kMW0wZ-tp9K5PoIsV415JXureRCxIRYLop5wVdRxGmQIvevVBDpFggw=s48-c-k-c0x00ffffff-no-rj"
   },
   {
     _id: uuidv4(),
-    title: "2 Pros Vs 5 Noobs in Valorant",
+    title: "Character Demo - Zhongli: The Listener",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "One of the best character Demos from genshin second only to Baals of course. The god of liyue can be seen in all his grace and play style",
     videoUrl: "https://www.youtube.com/watch?v=oJSdnwbFLVo",
-    channelName: "TenZ",
-    category: "Funny Clips",
-    thumbnail: "https://img.youtube.com/vi/oJSdnwbFLVo/maxresdefault.jpg"
+    channelName: "Genshin Impact",
+    category: "New Updates",
+    thumbnail: "https://img.youtube.com/vi/4oBpaBEMBIM/maxresdefault.jpg",
+    channelImg: "https://yt3.ggpht.com/7FlKwMyotApIow-_l6-kMW0wZ-tp9K5PoIsV415JXureRCxIRYLop5wVdRxGmQIvevVBDpFggw=s48-c-k-c0x00ffffff-no-rj"
   },
   {
     _id: uuidv4(),
-    title: "2 Pros Vs 5 Noobs in Valorant",
+    title: "Cold ❄️ (Valorant Montage)",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    videoUrl: "https://www.youtube.com/watch?v=oJSdnwbFLVo",
-    channelName: "TenZ",
-    category: "Funny Clips",
-    thumbnail: "https://img.youtube.com/vi/oJSdnwbFLVo/maxresdefault.jpg"
+      "A super cool (see what i did there ;)) montage with precise aim and inhuman gameplay.",
+    videoUrl: "https://www.youtube.com/watch?v=8KKGT4JXPVY",
+    channelName: "shawnlol",
+    category: "Montages",
+    thumbnail: "https://img.youtube.com/vi/8KKGT4JXPVY/maxresdefault.jpg",
+    channelImg: "https://yt3.ggpht.com/OHdwvDvIpkUFKkapJ38Nx5U1EW5UlsgyWLIXqyQqURo5XqHMZ3krOiCYHnYaE-AOGGU9zOtq=s48-c-k-c0x00ffffff-no-rj"
+  },
+  {
+    _id: uuidv4(),
+    title: "Grand final! sentinels Vs 100T",
+    description:
+      "The Grand finale of sentinels versus 100T. this includes moments of friendly flashes and TenZ playing Reyna what else could you ask for? ",
+    videoUrl: "https://www.youtube.com/watch?v=jcY60vL7208",
+    channelName: "VALORANT VODS",
+    category: "Tournaments",
+    thumbnail: "https://img.youtube.com/vi/jcY60vL7208/maxresdefault.jpg",
+    channelImg: "https://yt3.ggpht.com/BJv8aohVrRKkA-6xeAx-hh8nCJWFLhcZl2QDgR3PfI3nP6hm_aNVljk1mGqXJ_x4bbWM5Auo=s88-c-k-c0x00ffffff-no-rj"
   },
 ];
